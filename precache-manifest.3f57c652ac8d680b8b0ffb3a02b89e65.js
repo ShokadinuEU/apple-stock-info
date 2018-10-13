77f7991bb0cf957dc1c36f636ecbb1460b16c598
Timotei Dumitru <dumitru.timy@gmail.com>
2018-10-13 17:23:12 +0100

    Updates

---
+++ precache-manifest.3f57c652ac8d680b8b0ffb3a02b89e65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "20a9d080a5111c4cde8a",
+    "url": "/apple-stock-info/static/js/runtime~main.20a9d080.js"
+  },
+  {
+    "revision": "f4af96968caeb4c89c40",
+    "url": "/apple-stock-info/static/js/main.f4af9696.chunk.js"
+  },
+  {
+    "revision": "e097cbb0e5ec7dc21d4f",
+    "url": "/apple-stock-info/static/js/1.e097cbb0.chunk.js"
+  },
+  {
+    "revision": "f4af96968caeb4c89c40",
+    "url": "/apple-stock-info/static/css/main.1c3b4f49.chunk.css"
+  },
+  {
+    "revision": "545bc335913943f2f39303f0f352ca74",
+    "url": "/apple-stock-info/index.html"
+  }
+];
