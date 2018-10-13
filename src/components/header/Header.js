2dd5d588640c84bf51127891b00e0c98571e6e58
Timotei Dumitru <dumitru.timy@gmail.com>
2018-10-13 11:33:36 +0100

    implement header slider and made evelithing more responsive

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import './header.scss'
+import {Carousel} from 'react-bootstrap'
 
 const Header = () => {
   return (
     <header className="main-header">
-         <h1>Apple Inc.</h1>
+      <h1 className="title-header">Apple Inc.</h1>
+      <div className="slider-header">
+        <Carousel>
+          <Carousel.Item>
+            <img  height={200} alt="1" src="./img/img1.jpg" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img height={200} alt="2" src="./img/img2.jpg" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img height={200} alt="3" src="./img/img3.jpg" />
+          </Carousel.Item>
+        </Carousel>
+      </div>
     </header>
   )
 }
