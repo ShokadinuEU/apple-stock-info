6efe0cc8cce506b33679b696641350a36e90f244
Timotei Dumitru <dumitru.timy@gmail.com>
2018-10-13 13:51:07 +0100

    few more fixes on info panels

--- src/components/panels/Panels.js
+++ src/components/panels/Panels.js
@@ -3,28 +3,34 @@ import './panels.scss'
 
 const Panels = () => {
   return (
-    <panels className="panel-main">
+    <article className="panel-main">
       <div className="panel-container">
         <div className="panel-block">
-          <span className="image-panel">Image</span>
-          <h3 className="header-panel">Apple Infos</h3>
-          <p className="text-panel">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil provident nemo id.</p>
-          <button>More here ></button>
+          <i className="image-panel fas fa-globe-americas fa-3x"></i>
+          <div className="info-panel">
+            <h3 className="header-panel">Apple Infos</h3>
+            <p className="text-panel">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil provident nemo id.</p>
+            <button>More here ></button>
+          </div>
         </div>
         <div className="panel-block">
-          <span className="image-panel">Image</span>
-          <h3 className="header-panel">Apple Infos</h3>
-          <p className="text-panel">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil provident nemo id.</p>
-          <button>More here ></button>
+          <i className="image-panel fas fa-cogs fa-3x"></i>
+          <div className="info-panel">
+            <h3 className="header-panel">Apple Infos</h3>
+            <p className="text-panel">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil provident nemo id.</p>
+            <button>More here ></button>
+          </div>
         </div>
         <div className="panel-block">
-          <span className="image-panel">Image</span>
-          <h3 className="header-panel">Apple Infos</h3>
-          <p className="text-panel">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil provident nemo id.</p>
-          <button>More here ></button>
+          <i className="image-panel far fa-lightbulb fa-3x"></i>
+          <div className="info-panel">
+            <h3 className="header-panel">Apple Infos</h3>
+            <p className="text-panel">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil provident nemo id.</p>
+            <button>More here ></button>
+          </div>
         </div>
       </div>
-    </panels>
+    </article>
   )
 }
 
