914c185be045356dc03ddd1439fd34a21e439d8d
Timotei Dumitru <dumitru.timy@gmail.com>
2018-10-14 17:44:02 +0100

    fixed the small error in text editor that refers to redirecting of links from menu seection

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -7,11 +7,11 @@ const Menu = () => {
     <menu className="main-menu">
       <div className="nav-menu">
         <div className="link-menu">
-          <a href="#" className="item-menu">Home</a>
-          <a href="#" className="item-menu">About us</a>
-          <a href="#" className="item-menu">Products</a>
-          <a href="#" className="item-menu">Services</a>
-          <a href="#" className="item-menu">Contact</a>
+          <a href="/" className="item-menu">Home</a>
+          <a href="/" className="item-menu">About us</a>
+          <a href="/" className="item-menu">Products</a>
+          <a href="/" className="item-menu">Services</a>
+          <a href="/" className="item-menu">Contact</a>
         </div>
         <div className="search-menu">
           <input placeholder="look for more.." type="text"/>
